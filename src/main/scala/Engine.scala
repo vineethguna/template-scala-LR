4dfd3c085b18077413aaf4d4466c95e3450ce09b
vineethguna <vineeth.guna@gmail.com>
2016-09-02 12:36:36 +0530

    Initial Commit

---
+++ src/main/scala/Engine.scala
@@ -0,0 +1,26 @@
+package org.template.classification
+
+import io.prediction.controller.EngineFactory
+import io.prediction.controller.Engine
+
+class Query(
+  val features : Array[Double]
+) extends Serializable
+
+class PredictedResult(
+  val label: Double
+) extends Serializable
+
+class ActualResult(
+  val label: Double
+) extends Serializable
+
+object ClassificationEngine extends EngineFactory {
+  def apply() = {
+    new Engine(
+      classOf[DataSource],
+      classOf[Preparator],
+      Map("LR" -> classOf[LogisticRegressionAlgorithm]),
+      classOf[Serving])
+  }
+}
---
+++ src/main/scala/LogisticRegressionAlgorithm.scala
@@ -0,0 +1,41 @@
+package org.template.classification
+
+import io.prediction.controller.P2LAlgorithm
+import io.prediction.controller.Params
+
+import org.apache.spark.mllib.classification.LogisticRegressionWithLBFGS
+import org.apache.spark.mllib.classification.LogisticRegressionModel
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.SparkContext
+
+import grizzled.slf4j.Logger
+
+case class AlgorithmParams(
+  maxIter: Int,
+  regParam: Double,
+  fitIntercept: Boolean,
+  categoricalPrediction: Boolean
+) extends Params
+
+// extends P2LAlgorithm because the MLlib's NaiveBayesModel doesn't contain RDD.
+class LogisticRegressionAlgorithm()
+  extends P2LAlgorithm[PreparedData, LogisticRegressionModel, Query, PredictedResult] {
+
+  @transient lazy val logger = Logger[this.type]
+
+  def train(sc: SparkContext, data: PreparedData): LogisticRegressionModel = {
+    // MLLib NaiveBayes cannot handle empty training data.
+    require(data.labeledPoints.take(1).nonEmpty,
+      s"RDD[labeledPoints] in PreparedData cannot be empty." +
+      " Please check if DataSource generates TrainingData" +
+      " and Preparator generates PreparedData correctly.")
+    new LogisticRegressionWithLBFGS().run(data.labeledPoints)
+    //NaiveBayes.train(data.labeledPoints, ap.lambda)
+  }
+
+  def predict(model: LogisticRegressionModel, query: Query): PredictedResult = {
+    val label = model.predict(Vectors.dense(query.features))
+    new PredictedResult(label)
+  }
+
+}
